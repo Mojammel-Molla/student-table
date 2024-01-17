96671085eb7468486592b4c82e2af87ad793db67
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-17 13:08:17 +0600

    Footer section Created

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,18 @@
+import Footer from '../home-components/footer/Footer';
 import Navbar from '../home-components/navbar/Navbar';
 import Banner from './../home-components/banner/Banner';
 
 const Home = () => {
   return (
-    <div className="bg-[#127958] ">
-      <Navbar></Navbar>
-      <Banner></Banner>
-    </div>
+    <>
+      <div className="bg-[#127958] ">
+        <Navbar></Navbar>
+        <Banner></Banner>
+      </div>
+      <div className="bg-[#172227]">
+        <Footer></Footer>
+      </div>
+    </>
   );
 };
 
---
+++ src/home-components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="max-w-7xl mx-auto ">
+      <p className="text-center text-base text-gray-500">
+        Copyright ©2024 | All rights reserved by Learn with Sumit
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
